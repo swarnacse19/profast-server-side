dfb026fcc3d26570fbe399e6b14f351a49226d28
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-04 16:47:29 +0600

    create get and post api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
@@ -9,6 +10,44 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saov0by.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('parcelDB');
+    const parcelCollection = db.collection('parcels');
+
+    app.get('/parcels', async(req, res) =>{
+      const result = await parcelCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/parcels', async(req, res) =>{
+      const newParcel = req.body;
+      const result = await parcelCollection.insertOne(newParcel);
+      res.send(result);
+    })
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) =>{
     res.send('parcel server is running');

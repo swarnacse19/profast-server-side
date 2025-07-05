fc605956a41161b3f6174780e5c2c9ca8de732e4
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-05 23:31:04 +0600

    create an api for tracking

--- index.js
+++ index.js
@@ -71,6 +71,28 @@ async function run() {
       }
     });
 
+    app.post("/tracking", async (req, res) => {
+      const {
+        tracking_id,
+        parcel_id,
+        status,
+        message,
+        updated_by = "",
+      } = req.body;
+
+      const log = {
+        tracking_id,
+        parcel_id: parcel_id ? new ObjectId(parcel_id) : undefined,
+        status,
+        message,
+        time: new Date(),
+        updated_by,
+      };
+
+      const result = await trackingCollection.insertOne(log);
+      res.send({ success: true, insertedId: result.insertedId });
+    });
+
     app.get("/payments", async (req, res) => {
       try {
         const userEmail = req.query.email;

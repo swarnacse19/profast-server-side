1af758011bee9ae2929a657d3be64ff800c6a8d8
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-06 23:37:14 +0600

    successfully connect to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,256 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const admin = require("firebase-admin");
 
 dotenv.config();
+
+// const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// const serviceAccount = require("./firebase-adminsdk.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saov0by.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("parcelDB");
+    const usersCollection = db.collection("users");
+    const parcelCollection = db.collection("parcels");
+    const paymentsCollection = db.collection("payments");
+    const ridersCollection = db.collection('riders');
+
+    // custom middlewares
+    // const verifyFBToken = async (req, res, next) => {
+    //   const authHeader = req.headers.authorization;
+    //   if (!authHeader) {
+    //     return res.status(401).send({ message: "unauthorized access" });
+    //   }
+    //   const token = authHeader.split(" ")[1];
+    //   console.log("token in the middleware", token);
+    //   if (!token) {
+    //     return res.status(401).send({ message: "unauthorized access" });
+    //   }
+
+    //   // verify the token
+    //   try {
+    //     const decoded = await admin.auth().verifyIdToken(token);
+    //     req.decoded = decoded;
+    //     next();
+    //   } catch (error) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    // };
+
+    // app.post("/users", async (req, res) => {
+    //   const email = req.body.email;
+    //   const userExists = await usersCollection.findOne({ email });
+    //   if (userExists) {
+    //     // update last log in
+    //     return res
+    //       .status(200)
+    //       .send({ message: "User already exists", inserted: false });
+    //   }
+    //   const user = req.body;
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    // });
+
+    app.get("/parcels", async (req, res) => {
+      try {
+        const email = req.query.email;
+
+        let query = {};
+        if (email) {
+          query = { created_by: email };
+        }
+
+        const parcels = await parcelCollection
+          .find(query)
+          .sort({ creation_date: -1 })
+          .toArray();
+
+        res.send(parcels);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
+
+    // app.get("/parcels/:id", async (req, res) => {
+    //   try {
+    //     const id = req.params.id;
+
+    //     const parcel = await parcelCollection.findOne({
+    //       _id: new ObjectId(id),
+    //     });
+
+    //     if (parcel) {
+    //       res.send(parcel);
+    //     } else {
+    //       res.status(404).send({ message: "Parcel not found" });
+    //     }
+    //   } catch (err) {
+    //     console.error(err);
+    //     res.status(500).send({ message: "Server error" });
+    //   }
+    // });
+
+    // app.post("/tracking", async (req, res) => {
+    //   const {
+    //     tracking_id,
+    //     parcel_id,
+    //     status,
+    //     message,
+    //     updated_by = "",
+    //   } = req.body;
+
+    //   const log = {
+    //     tracking_id,
+    //     parcel_id: parcel_id ? new ObjectId(parcel_id) : undefined,
+    //     status,
+    //     message,
+    //     time: new Date(),
+    //     updated_by,
+    //   };
+
+    //   const result = await trackingCollection.insertOne(log);
+    //   res.send({ success: true, insertedId: result.insertedId });
+    // });
+
+    // app.post("/riders", async (req, res) => {
+    //   const rider = req.body;
+    //   const result = await ridersCollection.insertOne(rider);
+    //   res.send(result);
+    // });
+
+    // app.get("/payments", verifyFBToken, async (req, res) => {
+    //   try {
+    //     const userEmail = req.query.email;
+
+    //     const query = userEmail ? { email: userEmail } : {};
+    //     const options = { sort: { paid_at: -1 } }; // Latest first
+
+    //     const payments = await paymentsCollection
+    //       .find(query, options)
+    //       .toArray();
+    //     res.send(payments);
+    //   } catch (error) {
+    //     console.error("Error fetching payment history:", error);
+    //     res.status(500).send({ message: "Failed to get payments" });
+    //   }
+    // });
+
+    app.post("/parcels", async (req, res) => {
+      const newParcel = req.body;
+      const result = await parcelCollection.insertOne(newParcel);
+      res.send(result);
+    });
+
+    // app.delete("/parcels/:id", async (req, res) => {
+    //   try {
+    //     const id = req.params.id;
+
+    //     const result = await parcelCollection.deleteOne({
+    //       _id: new ObjectId(id),
+    //     });
+
+    //     res.send(result);
+    //   } catch (err) {
+    //     console.error(err);
+    //     res.status(500).send({ success: false, message: "Server error" });
+    //   }
+    // });
+
+    // POST: Record payment and update parcel status
+    // app.post("/payments", async (req, res) => {
+    //   try {
+    //     const { parcelId, email, amount, paymentMethod, transactionId } =
+    //       req.body;
+
+    //     // 1. Update parcel's payment_status
+    //     const updateResult = await parcelCollection.updateOne(
+    //       { _id: new ObjectId(parcelId) },
+    //       {
+    //         $set: {
+    //           payment_status: "paid",
+    //         },
+    //       }
+    //     );
+
+    //     if (updateResult.modifiedCount === 0) {
+    //       return res
+    //         .status(404)
+    //         .send({ message: "Parcel not found or already paid" });
+    //     }
+
+    //     // 2. Insert payment record
+    //     const paymentDoc = {
+    //       parcelId,
+    //       email,
+    //       amount,
+    //       paymentMethod,
+    //       transactionId,
+    //       paid_at_string: new Date().toISOString(),
+    //       paid_at: new Date(),
+    //     };
+
+    //     const paymentResult = await paymentsCollection.insertOne(paymentDoc);
+
+    //     res.status(201).send({
+    //       message: "Payment recorded and parcel marked as paid",
+    //       insertedId: paymentResult.insertedId,
+    //     });
+    //   } catch (error) {
+    //     console.error("Payment processing failed:", error);
+    //     res.status(500).send({ message: "Failed to record payment" });
+    //   }
+    // });
+
+    // app.post("/create-payment-intent", async (req, res) => {
+    //   const amountInCents = req.body.amountInCents;
+    //   try {
+    //     const paymentIntent = await stripe.paymentIntents.create({
+    //       amount: amountInCents, // Amount in cents
+    //       currency: "usd",
+    //       payment_method_types: ["card"],
+    //     });
+
+    //     res.json({ clientSecret: paymentIntent.client_secret });
+    //   } catch (error) {
+    //     res.status(500).json({ error: error.message });
+    //   }
+    // });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("parcel server is running");
